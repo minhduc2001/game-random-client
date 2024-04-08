81fc2adedc43814f099a3f36df79f9c3be4d880a
ducnm <ngoduc2468@gmail.com>
2024-04-08 11:04:17 +0700

    test build change

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -2,6 +2,7 @@ import { Button, Col, Divider, Row } from 'antd';
 import Helmet from '../../components/Helmet';
 import AsyncWrapper from '../../layouts/AsyncWrapper';
 import './index.scss';
+import { MdLogin } from 'react-icons/md';
 
 function Homepage() {
   const isFetching = false;
@@ -18,17 +19,38 @@ function Homepage() {
             <Button>Xem hướng dẫn</Button>
             <Button>Nhóm Zalo</Button>
           </div>
-
-          <div className=''>aaaa</div>
-
           <Row gutter={22} className='row-data' wrap>
             <Col span={12}>
               <div className='row-data-game'>
-                <span className='row-header'>Game</span>
+                <span className='row-header'>
+                  <>Game (Thường)</>
+                </span>
                 <Divider></Divider>
               </div>
             </Col>
-            <Col span={12}></Col>
+            <Col span={12}>
+              <div className='row-data-game'>
+                <span className='row-header'>
+                  <p>Game</p>
+                  <p>Số dư: 0 xèng</p>
+                </span>
+
+                <div className='panel-body'>
+                  <div className='alert-danger'>
+                    <p>Hãy đăng nhập để có thể đặt cược nhé</p>
+                    <p className='btn-login'>
+                      <a
+                        href='https://nsocltx.com/dang-nhap'
+                        className='btn btn-xs btn-danger'
+                      >
+                        <MdLogin />
+                        &nbsp;&nbsp;Đăng nhập
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Col>
           </Row>
         </div>
       </div>
